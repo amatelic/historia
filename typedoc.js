b13d1541ec15de26aed113a4247c11a78293d39b
Anže Matelič <amatelic93@gmail.com>
2019-03-03 15:46:00 +0100

    Added code for generating typescript documentation

---
+++ typedoc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "Historia",
+  excludePrivate: true,
+  exclude: '**/node_modules/**',
+  includeDeclarations: true,
+  excludeExternals: true,
+  mode: 'file',
+  out: "./docs",
+  verbose: true
+};
